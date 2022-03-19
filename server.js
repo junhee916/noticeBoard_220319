ea8a6fa49faee2b9111539a066755e932f2c6bc5
junhee916 <junhee916@naver.com>
2022-03-19 18:09:49 +0900

    update board and commend

--- server.js
+++ server.js
@@ -4,12 +4,17 @@ const app = express()
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
 const userRouter = require('./routes/user')
+const boardRouter = require('./routes/board')
+const commendRouter = require('./routes/commend')
 const connectDB = require('./config/database')
 connectDB()
 // middleware
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended : false}))
 app.use(morgan('dev'))
+// connect router
 app.use('/user', userRouter)
+app.use('/board', boardRouter)
+app.use('/commend', commendRouter)
 const PORT = process.env.PORT || 7000
 app.listen(PORT, console.log("connected server..."))
